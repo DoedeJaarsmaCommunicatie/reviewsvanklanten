924815d1fb411d3a77a6b4cf4b7c2c9c33bede34
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-08-28 13:10:06 +0200

    Added search for companies

--- app/Http/Controllers/Api/V1/Companies/Fetch.php
+++ app/Http/Controllers/Api/V1/Companies/Fetch.php
@@ -3,13 +3,69 @@
 namespace App\Http\Controllers\Api\V1\Companies;
 
 use App\Models\Company;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CompanyResource;
 
 class Fetch extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
+        if ($this->hasRequestKey($request, $this->getUuidRequestKey())) {
+            $company = Company::whereUuid($this->getRequestValueOnKey($request, $this->getUuidRequestKey()));
+
+            if ($company->exists()) {
+                return CompanyResource::collection($company->get());
+            }
+
+            return response()->json(['error' => '404', 'message' => 'Company with hash not found', 'data' => [
+                'hash' => $this->getRequestValueOnKey($request, $this->getUuidRequestKey()),
+            ]], 404);
+        }
+
+        if ($this->hasRequestKey($request, $this->getSearchRequestKey())) {
+            $company = Company::where('name', 'LIKE', "%{$this->getRequestValueOnKey($request, $this->getSearchRequestKey())}%");
+
+            if (!$company->exists()) {
+                return response()->json(
+                    [
+                        'error' => '404',
+                        'message' => 'No results for company found',
+                        'data' => [
+                            'query' => $this->getRequestValueOnKey($request, $this->getSearchRequestKey()),
+                        ],
+                    ],
+                    404
+                );
+            }
+
+            return CompanyResource::collection($company->get());
+        }
+
         return CompanyResource::collection(Company::all());
+    }
+
+    protected function getRequestValueOnKey(Request $request, string $key)
+    {
+        if (!$this->hasRequestKey($request, $key)) {
+            return null;
+        }
+
+        return $request->get($key);
+    }
+
+    protected function hasRequestKey(Request $request, string $key): bool
+    {
+        return $request->has($key);
+    }
+
+    private function getUuidRequestKey(): string
+    {
+        return 'hash';
+    }
+
+    private function getSearchRequestKey(): string
+    {
+        return 'search';
     }
 }
