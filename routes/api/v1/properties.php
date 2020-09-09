e46b28ba71e352ae5d6ceda5b26db326ffc5100c
Mitch Hijlkema <6103741+Pridestalker@users.noreply.github.com>
2020-09-09 13:08:14 +0200

    Removed unfinished routes

--- routes/api/v1/properties.php
+++ routes/api/v1/properties.php
@@ -13,12 +13,12 @@ Route::get('single/{id}', 'Api\\V1\\Properties\\Single@index')
     ->name('single.id');
 
 Route::prefix('review')->group(static function () {
-    Route::get('/', '')
-         ->name('reviews.fetch');
+//    Route::get('/', '')
+//         ->name('reviews.fetch');
 
     Route::post('/', 'Api\\V1\\Reviews\\Property\\CreateReviewController@index')
         ->name('review.create');
 
-    Route::get('/latest', '')
-        ->name('review.fetch.latest');
+//    Route::get('/latest', '')
+//        ->name('review.fetch.latest');
 });
